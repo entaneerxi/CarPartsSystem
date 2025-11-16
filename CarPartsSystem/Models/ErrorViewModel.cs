40bb9a9d9ded1ac4894491918d4132cf3a4ae23d
Gibson <Gibson@DESKTOP-1N1TLES>
2025-11-16 11:09:58 +0700

    Add project files.

---
+++ CarPartsSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarPartsSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
